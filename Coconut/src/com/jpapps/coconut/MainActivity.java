5ac775f458b103e3c5f76953541851789dd30477
'Paul <thesamuraipanda@gmail.com>
2013-07-21 13:57:32 -0400

    Added click and longclick functionality to coconut button.

--- Coconut/src/com/jpapps/coconut/MainActivity.java
+++ Coconut/src/com/jpapps/coconut/MainActivity.java
@@ -8,23 +8,45 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	MediaPlayer coconutMP, stopMP;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		MediaPlayer coconutMP = new MediaPlayer().create(this, R.raw.coconut);
-		MediaPlayer stopMP = new MediaPlayer().create(this, R.raw.stopping);
+		coconutMP = new MediaPlayer().create(this, R.raw.coconut);
+		stopMP = new MediaPlayer().create(this, R.raw.stopping);
 		
 		Button coconutButton = (Button) this.findViewById(R.id.button_coconut);
 		coconutButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
+				if(coconutMP.isPlaying()) {
+					coconutMP.seekTo(0);
+				} else {
+					coconutMP.start();
+				}
+			}
+		});
+		coconutButton.setOnLongClickListener(new OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				if(coconutMP.isPlaying()) {
+					coconutMP.pause();
+					coconutMP.seekTo(0);
+				}
+				if(stopMP.isPlaying()) {
+					stopMP.seekTo(0);
+				} else {
+					stopMP.start();
+				}
+				return true;
 			}
 		});
 	}
