086051d0611716bb39b36ba3ddb1ed6bcf8b62a7
'Paul <thesamuraipanda@gmail.com>
2013-07-21 13:34:54 -0400

    Added coconut image to main layout. Starting to set up MPs & listeners.

--- Coconut/src/com/jpapps/coconut/MainActivity.java
+++ Coconut/src/com/jpapps/coconut/MainActivity.java
@@ -2,9 +2,13 @@ package com.jpapps.coconut;
 
 import com.example.coconut.R;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,13 +16,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		
+		MediaPlayer coconutMP = new MediaPlayer().create(this, R.raw.coconut);
+		MediaPlayer stopMP = new MediaPlayer().create(this, R.raw.stopping);
+		
+		Button coconutButton = (Button) this.findViewById(R.id.button_coconut);
+		coconutButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
 	}
 
 }
